ab0ffb0663602bbbcb83651af6db906aac7b72c4
esalling23 <esalling23@gmail.com>
2016-07-31 19:29:59 -0400

    implemented template loader

--- routes/api/login.js
+++ routes/api/login.js
@@ -1,23 +1,43 @@
 var keystone = require('keystone');
 var Player = keystone.list('Player');
+var appRoot = require('app-root-path');
+var TemplateLoader = require('../../lib/TemplateLoader');
+var Templates = new TemplateLoader();
 
 exports.get = function(req, res) {
 
-	console.log (req, "req");
+	// console.log (req, "req");
+
 
 
 	var query = Player.model.findOne({$or:[{userName:req.query.username},{email:req.query.username}]});
 
-	console.log(query, "one");
-
 	query.select('userName email pass');
 
-	console.log(query);
-
 	query.exec(function (err, person) {
-		console.log (person);
+		console.log ("person:" + person + ":person");
 	  if (err) return handleError(err);
-	  console.log('This is %s with password %s and email %s.', person.userName, person.pass, person.email); // Space Ghost is a talk show host.
+	  console.log('This is %s with password %s and email %s.', person.userName, person.pass, person.email);
+	  var data = {username:person.userName, password:person.pass, email:person.email}
+	  if (person.pass === req.query.password) {
+
+	  	console.log("correct password");
+
+	  	// locals.username = person.userName;
+	  	// locals.password = person.pass;
+	  	// locals.email = person.email;
+	  	
+
+	  	Templates.Load("partials/profile", data, function(html) {
+	  		console.log(html, " --- html --- ");
+	  		// console.log(data, " --- html --- ");
+            res.send(html);
+
+        }); 
+
+	  } else {
+	  	console.log("wrong password");
+	  }
 	});
-	
+
 };
