5dbe0ff78bb35faba11b36305b0571a7c881e85d
esalling23 <esalling23@gmail.com>
2016-09-07 15:01:41 -0400

    resource model

---
+++ models/Resource.js
@@ -0,0 +1,21 @@
+var keystone = require('keystone'),
+    Types = keystone.Field.Types;
+ 
+var Player = new keystone.List('Player', {
+	label: 'Players',
+		singular: 'Player',
+		track: true,
+		// nodelete: true,
+		nocreate: false
+});
+ 
+Player.add({
+    userName: { type: String, required: true, initial: true, index: true },
+    email: { type: Types.Email, initial: true, required: true, index: true },
+    pass: { type: String }
+    // visited: { type: Types.Relationship}
+    // resources: { types: Types.Relationship, ref: }
+});
+ 
+Player.register();
+// module.exports = keystone.model('Player', Player);
